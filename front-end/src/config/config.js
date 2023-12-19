ecb3d6da0409bc65bd20cfb6f372eac4931a6642
pninayonayov <pninayo@edu.hac.ac.il>
2023-12-19 10:31:51 +0200

    new backend url for deploy

--- front-end/src/config/config.js
+++ front-end/src/config/config.js
@@ -1,4 +1,4 @@
 const BASE_URL = process.env.NODE_ENV === 'production'
-    ? 'https://code-blocks-z5h4.onrender.com'
+    ? 'https://code-blocks-xiw5.onrender.com'
     : 'http://localhost:5000';
 export default BASE_URL;
